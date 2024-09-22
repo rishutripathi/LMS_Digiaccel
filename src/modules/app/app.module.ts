60d3d3dd1d8d9b45df34224a103e46ab2fdc2ac2
Rishu Tripathi <rishutripathi@Rishus-MacBook-Air.local>
2024-09-22 08:04:02 +0530

    volume mounting in docker-corrected

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -11,7 +11,9 @@ import * as path from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     DatabaseModule,
     AuthModule,
     TestModule,
